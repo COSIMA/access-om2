ee758c0cdecffe7c87b5e0bc5ed464fcba8ac419
Nicholas Hannah <nchlshnnh@gmail.com>
2014-08-11 16:22:01 +1000

    Do experiments clone from within setup script instead of getting Jenkins to do it. Minimise Jenkins config.

--- ci_setup_lab.py
+++ ci_setup_lab.py
@@ -3,6 +3,8 @@
 import sys
 import os, errno
 import shutil
+import shlex
+import subprocess as sp
 
 """
 Set up the payu lab and input directories before running tests. 
@@ -16,7 +18,26 @@ def mkdir_p(path):
             pass
         else: raise
 
+
+def clone(repo_url, dest):
+
+    # Clone the experiments directory. 
+    path = os.path.join(dest, '.git')
+    if not os.path.exists(path):
+        cmd = 'git clone {} {}'.format(repo_url, dest)
+        sp.check_call(shlex.split(cmd))
+
+    curdir = os.getcwd()
+    os.chdir(dest)
+    sp.check_call(shlex.split('git checkout master'))
+    sp.check_call(shlex.split('git pull'))
+    os.chdir(curdir)
+
+
 def main():
+
+    clone('https://github.com/nicholash/payu-experiments.git',
+          'payu-experiments')
 
     mkdir_p('lab/archive')
     mkdir_p('lab/bin')
