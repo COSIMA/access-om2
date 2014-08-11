8626279c48fd63cdae01a0875f3e88410b8b5049
Nicholas Hannah <nchlshnnh@gmail.com>
2014-08-11 17:45:22 +1000

    Enable more tests. Many won't be run because they are slow.

--- test_run.py
+++ test_run.py
@@ -1,6 +1,7 @@
 
 from __future__ import print_function
 
+from nose.plugins.attrib import attr
 from model_test_setup import ModelTestSetup
 import subprocess as sp
 import shutil
@@ -78,8 +79,7 @@ class TestRun(ModelTestSetup):
         Run the ACCESS-CM_tiny experiment.
         """
 
-        #self.do_basic_run('access-cm_tiny')
-        pass
+        self.do_basic_run('access-cm_tiny')
 
 
     def test_ACCESS_OM(self):
@@ -87,8 +87,7 @@ class TestRun(ModelTestSetup):
         Run the ACCESS-OM experiment.
         """
 
-        #self.do_basic_run('access-om')
-        pass
+        self.do_basic_run('access-om')
 
 
     def test_ACCESS_CM(self):
@@ -96,22 +95,21 @@ class TestRun(ModelTestSetup):
         Run the ACCESS-CM experiment.
         """
 
-        #self.do_basic_run('access-cm')
-        pass
+        self.do_basic_run('access-cm')
 
 
+    @attr('slow')
     def test_ACCESS_OM_1440x1080(self):
         """
         Run the ACCESS-OM_1440x1080 experiment.
         """
 
-        #self.do_basic_run('access-om_1440x1080')
-        pass
+        self.do_basic_run('access-om_1440x1080')
 
+    @attr('slow')
     def test_ACCESS_CM_1440x1080(self):
         """
         Run the ACCESS-CM_1440x1080 experiment.
         """
 
-        #self.do_basic_run('access-cm_1440x1080')
-        pass
+        self.do_basic_run('access-cm_1440x1080')
