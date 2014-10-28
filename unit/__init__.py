88a83d8cb5c43e6097942764a0a72410af6855ee
Nicholas Hannah <nchlshnnh@gmail.com>
2014-10-29 08:44:09 +1100

    Unit testing runs don't need to succeed, just to dump something.

--- unit/__init__.py
+++ unit/__init__.py
@@ -11,12 +11,11 @@ def setup():
     helper = ModelTestHelper()
 
     # Build and run models with unit testing enabled.  
-    exps = ['access-cm_1440x1080-unit_test', 'access-cm-unit_test']
+    exps = ['cm_1440x1080-unit_test', 'om_1440x1080-unit_test', 'cm-unit_test']
     for e in exps:
         paths = helper.make_paths(e)
         ret = helper.build(paths['exp'])
         assert(ret == 0)
         ret, _, _, _ = helper.run(paths['exp'], helper.lab_path) 
-        assert(ret == 0)
 
-    # The dumped files should now all be in place. 
+    # Check that there are some dumped files in place. 
