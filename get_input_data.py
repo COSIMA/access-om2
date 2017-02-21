dc61497b3780f5225b71bdd70e9e1db1dca69ed0
Nicholas Hannah <nicjhannah@gmail.com>
2017-02-21 15:34:55 +1100

    Provide alternative source for datasets.

--- get_input_data.py
+++ get_input_data.py
@@ -3,18 +3,27 @@
 import sys
 import os, errno
 import shlex
+import sh
 import subprocess as sp
 import argparse
 
+data_path = '/short/public/access-om/access-om.tar.gz'
+data_url = 'http://s3-ap-southeast-2.amazonaws.com/dp-drop/access-om/access-om.tar.gz'
+
 def main():
 
+    my_dir = os.path.dirname(os.path.realpath(__file__))
+    tarball = os.path.join(my_dir, 'access-om.tar.gz')
+
     # Download input data.
-    data = '/short/public/access-om/access-om.tar.gz'
+    if not os.path.exists(data_path):
+        sh.wget('-P', my_dir, data_url)
+    else:
+        sh.cp(data_path, my_dir)
 
-    cmd = '/bin/tar -zxvf {}'.format(data)
-    ret = sp.call(shlex.split(cmd))
-    return ret
+    sh.tar('zxvf', tarball, '-C', my_dir)
 
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main())
