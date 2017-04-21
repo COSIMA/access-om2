3052b3dbafd9505c329909c5fe766039d69cf1b4
Nicholas Hannah <nicjhannah@gmail.com>
2017-04-21 11:53:28 +1000

    Example runscript now sets up INPUT dir if necessary.

--- runscript.py
+++ runscript.py
@@ -64,10 +64,24 @@ def get_archive_dir(exp_dir, run_date):
         os.mkdir(archive_dir)
     return archive_dir
 
-def copy_files_around_before_run(exp_dir, run_date, run_type, input_dir):
+def copy_files_around_before_run(exp_dir, run_date, config_files, run_type,
+                                 initial_input_dir):
     """
-    Save the input directory for this run.
+    Possibly setup the INPUT directory. Save the input directory to be used by
+    this run..
     """
+
+    input_dir = os.path.join(exp_dir, 'INPUT')
+
+    if run_type == "'initial'":
+        shutil.copytree(initial_input_dir, input_dir, copy_function=shutil.copy)
+
+    for f in os.listdir(input_dir):
+        os.chmod(os.path.join(input_dir, f), stat.S_IRUSR | stat.S_IWUSR)
+
+    # Keep a copy of the configs in the INPUT for good measure
+    for c in config_files:
+        shutil.copy(c, input_dir)
 
     output_dir = os.path.join(exp_dir, 'OUTPUT')
     if not os.path.exists(output_dir):
@@ -78,16 +92,18 @@ def copy_files_around_before_run(exp_dir, run_date, run_type, input_dir):
     # Delete any old ice restart from input, otherwise they build up.
     ice_res = get_curr_cice_restart(exp_dir)
     if ice_res is not None:
-        for f in glob.glob(os.path.join(exp_dir, 'INPUT/iced.*.nc')):
-            if f != ice_res:
+        for f in glob.glob(os.path.join(input_dir, './iced.*.nc')):
+            if os.path.normpath(f) != os.path.normpath(ice_res):
                 os.remove(f)
 
     # Keep a copy of INPUT for later reference. We should be able to restart
     # from this.
-    for f in glob.glob(os.path.join(exp_dir, 'INPUT/*')):
-        os.chmod(f, stat.S_IRUSR | stat.S_IWUSR)
-    shutil.copytree(os.path.join(exp_dir, 'INPUT'),
-                    os.path.join(archive_dir, 'INPUT'))
+    shutil.copytree(input_dir, os.path.join(archive_dir, 'INPUT'))
+
+    try:
+        os.mkdir(os.path.join(exp_dir, 'RESTART'))
+    except FileExistsError:
+        pass
 
 def copy_files_around_after_run(exp_dir, run_date):
 
@@ -130,8 +146,9 @@ def init_namelists(exp_dir, timestep, runtime, curr_date,
     runtime_in_days = runtime_in_seconds // 86400
     runtime_in_ice_timesteps = runtime_in_seconds // ice_timestep
 
-    for config_file in glob.glob('{}/templates/*'.format(exp_dir)):
-        with open(config_file, 'r') as f:
+    configs = []
+    for template_file in glob.glob('{}/templates/*'.format(exp_dir)):
+        with open(template_file, 'r') as f:
             template = Template(f.read())
             s = template.render(ice_timestep=ice_timestep,
                                 ocn_timestep=ocn_timestep,
@@ -146,13 +163,12 @@ def init_namelists(exp_dir, timestep, runtime, curr_date,
                                 elapsed_time_in_seconds=elapsed_time_in_seconds,
                                 run_type=run_type)
 
-            fname = os.path.basename(config_file)
-            # Write a copy to INPUT as well.
-            with open(os.path.join(exp_dir, 'INPUT', fname), 'w') as of:
-                of.write(s)
-            with open(os.path.join(exp_dir, fname), 'w') as of:
+            config_file = os.path.join(exp_dir, os.path.basename(template_file))
+            configs.append(config_file)
+            with open(config_file, 'w') as of:
                 of.write(s)
 
+    return configs
 
 def get_cice_date(exp_dir):
     """
@@ -208,7 +224,7 @@ def main():
                         help='The ice and ocean timestep in seconds.')
     parser.add_argument('--runtime', type=int, default=86400,
                         help='The per-submit runtime in seconds.')
-    parser.add_argument('--input_dir', default='./input/1deg/',
+    parser.add_argument('--input_dir', default='./input/1deg/INPUT',
                         help='Input directory, relative to this script')
 
     args = parser.parse_args()
@@ -218,6 +234,8 @@ def main():
 
     top_dir = os.path.dirname(os.path.realpath(__file__))
     exp_dir = os.path.join(top_dir, args.experiment)
+    initial_input_dir = os.path.join(top_dir, args.input_dir)
+    input_dir = os.path.join(exp_dir, 'INPUT')
 
     mom_curr_date, mom_init_date, mom_caltype = get_mom_date(exp_dir)
     cice_curr_date, cice_elapsed_time_seconds = get_cice_date(exp_dir)
@@ -228,12 +246,14 @@ def main():
         run_type = "'continue'"
     else:
         run_type = "'initial'"
+        assert not os.path.exists(input_dir)
 
-    init_namelists(exp_dir, args.model_timestep, args.runtime, mom_curr_date,
-                   cice_elapsed_time_seconds, run_type)
+    configs = init_namelists(exp_dir, args.model_timestep, args.runtime,
+                             mom_curr_date, cice_elapsed_time_seconds, run_type)
 
-    copy_files_around_before_run(exp_dir, mom_curr_date, run_type,
-                                 args.input_dir)
+    copy_files_around_before_run(exp_dir, mom_curr_date, configs, run_type,
+                                 initial_input_dir)
+
     ret = run(args.experiment, top_dir)
 
     if ret == 0:
