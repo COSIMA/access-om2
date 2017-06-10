21fe609d6412da1396a136577572f8b2d93bb50e
Nicholas Hannah <nic@doubleprecision.com.au>
2017-06-10 23:52:52 +1000

    Get build tests working.

--- test/test_build.py
+++ test/test_build.py
@@ -4,34 +4,41 @@ from __future__ import print_function
 import subprocess as sp
 import os
 import shlex
+import pytest
 
 class TestBuild():
 
+    def build_oasis(self):
+        return sp.call(['make', '-C', 'src/oasis3-mct'])
+
     def test_build_matm(self):
 
-        mydir = os.getcwd()
-        os.chdir('src/matm')
+        ret = self.build_oasis()
+        assert ret == 0
+        os.environ['OASIS_ROOT'] = os.path.join(os.getcwd(), 'src/oasis3-mct')
 
-        ret = sp.call(['make'])
-        os.chdir(mydir)
+        ret = sp.call(['make', '-C', 'src/matm'])
         assert ret == 0
 
     def test_build_cice(self):
 
-        for res in ['1', '01', '025']:
-            mydir = os.getcwd()
-            os.chdir('src/cice5')
-            ret = sp.call(['make', 'access-om{}'.format(res)])
+        ret = self.build_oasis()
+        assert ret == 0
+        os.environ['OASIS_ROOT'] = os.path.join(os.getcwd(), 'src/oasis3-mct')
 
-            os.chdir(mydir)
+        for res in ['1', '01', '025']:
+            ret = sp.call(['make', '-C', 'src/cice5', 'access-om{}'.format(res)])
             assert ret == 0
 
     def test_build_mom(self):
 
-      mydir = os.getcwd()
-      os.chdir('src/mom/exp')
+        ret = self.build_oasis()
+        assert ret == 0
+        os.environ['OASIS_ROOT'] = os.path.join(os.getcwd(), 'src/oasis3-mct')
 
-      ret = sp.call(['./MOM_compile.csh', '--type', 'ACCESS-OM', '--platform', 'nci']) 
+        mydir = os.getcwd()
+        os.chdir('src/mom/exp')
+        ret = sp.call(['./MOM_compile.csh', '--type', 'ACCESS-OM', '--platform', 'nci'])
 
-      os.chdir(mydir)
-      assert ret == 0
+        os.chdir(mydir)
+        assert ret == 0
