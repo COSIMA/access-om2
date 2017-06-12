f049217ace544e11f95fd6a6d64860cccc9f7218
Nicholas Hannah <nic@doubleprecision.com.au>
2017-06-12 20:34:28 +1000

    Setup slow test run.

--- test/test_run.py
+++ test/test_run.py
@@ -8,12 +8,26 @@ class TestRun():
     Run and check return code.
     """
 
+    @pytest.mark.fast
     def test_run(self):
 
         ret = sp.call(['./get_input_data.py'])
         assert ret == 0
 
         helper = ExpTestHelper('1deg_jra55_ryf')
+        assert helper.build() == 0
+        ret, qso, qse, qsub_files = helper.run()
+
+        assert ret == 0
+
+
+    @pytest.mark.slow
+    def test_slow_run(self):
+
+        ret = sp.call(['./get_input_data.py'])
+        assert ret == 0
+
+        helper = ExpTestHelper('025deg_jra55_ryf')
         assert helper.build() == 0
         ret, qso, qse, qsub_files = helper.run()
 
