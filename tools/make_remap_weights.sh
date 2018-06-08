f8688b9868a968a3ebb734669bcacf5e05a1ce7a
Nic Hannah <nic@doubleprecision.com.au>
2018-06-08 13:42:50 +1000

    Support for creation of remapping weights between Daitren runoff and ice/ocean grid.

--- tools/make_remap_weights.sh
+++ tools/make_remap_weights.sh
@@ -4,6 +4,7 @@
 #PBS -l ncpus=256,mem=512,walltime=05:00:00,jobfs=100GB
 #PBS -l wd
 
-module load esmf/6.3.0rp1-intel
+module load esmf/7.1.0r-intel
 
-time ./make_remap_weights.py /short/x77/nah599/access-om2/input/ /g/data1/ua8/JRA55-do/RYF/v1-3/  --ocean MOM01 --npes 256
+# Make all 0.1 deg weights.
+time ./make_remap_weights.py /short/x77/nah599/access-om2/input/ /g/data1/ua8/JRA55-do/RYF/v1-3/ /short/x77/nah599/access-om2/input/yatm_1deg/ --ocean MOM01 --npes 256
