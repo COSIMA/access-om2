93cd260390ff9788994544341ee39ab4147fd6d9
Nic Hannah <nichannah@pm.me>
2018-11-21 14:53:31 +1100

    Update test markers to allow selection based on resolution.

--- test/test_build.py
+++ test/test_build.py
@@ -1,28 +1,34 @@
 
+import pytest
 from exp_test_helper import ExpTestHelper
 
 class TestBuild():
 
+    @pytest.mark.1deg
     def test_build_libaccessom2(self):
         helper = ExpTestHelper('1deg_jra55_ryf')
         exe, ret = helper.build_libaccessom2()
         assert ret == 0
 
+    @pytest.mark.1deg
     def test_build_1deg_cice(self):
         helper = ExpTestHelper('1deg_jra55_ryf')
         exe, ret = helper.build_cice()
         assert ret == 0
 
+    @pytest.mark.025deg
     def test_build_025deg_cice(self):
         helper = ExpTestHelper('025deg_jra55_ryf')
         exe, ret = helper.build_cice()
         assert ret == 0
 
+    @pytest.mark.01deg
     def test_build_01deg_cice(self):
         helper = ExpTestHelper('01deg_jra55_ryf')
         exe, ret = helper.build_cice()
         assert ret == 0
 
+    @pytest.mark.1deg
     def test_build_mom(self):
         helper = ExpTestHelper('1deg_jra55_ryf')
         exe, ret = helper.build_mom()
--- test/test_run.py
+++ test/test_run.py
@@ -9,22 +9,22 @@ class TestRun():
     Run and check return code.
     """
 
-    @pytest.mark.fast
+    @pytest.mark.1deg
     def test_1deg_jra55_ryf_run(self):
         run_exp('1deg_jra55_ryf')
 
-    @pytest.mark.slow
+    @pytest.mark.1deg
     def test_1deg_jra55_iaf_run(self):
         run_exp('1deg_jra55_iaf')
 
-    @pytest.mark.slow
+    @pytest.mark.1deg
     def test_1deg_core_run(self):
         run_exp('1deg_core_nyf')
 
-    @pytest.mark.slow
-    def test_slow_run(self):
+    @pytest.mark.025deg
+    def test_025deg_jra55_ryf_run(self):
         run_exp('025deg_jra55_ryf')
 
-    @pytest.mark.slow
-    def test_slow_run(self):
+    @pytest.mark.025deg
+    def test_025deg_jra55_iaf_run(self):
         run_exp('025deg_jra55_iaf')
